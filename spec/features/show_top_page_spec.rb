3fcb61c52f23ddaa94072adb0b02ca248d30326b
Katsuma Narisawa <face.the.music721@gmail.com>
2018-10-06 23:20:30 +0900

    Setup factories

--- spec/features/show_top_page_spec.rb
+++ spec/features/show_top_page_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'top page', type: :feature do
-  it 'show hello world' do
+  before do
+    create(:user, id: 1)
+    create(:user, id: 2)
+  end
+
+  it 'show user ids' do
     visit ''
     sleep(2)
 
-    expect(page).to have_content 'user ids: []'
+    expect(page).to have_content 'user ids: [ 1, 2 ]'
   end
 end
