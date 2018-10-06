8394bf9b7b5ebc4b482eee1ecb6f739a9a8ba769
Katsuma Narisawa <face.the.music721@gmail.com>
2018-10-06 17:11:40 +0900

    Run 'bin/rails generate scaffold users'

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_06_080935) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
