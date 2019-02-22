1d4b894b8dc45156d38460d46b915f430f8b95c2
Katsuma Narisawa <face.the.music721@gmail.com>
2019-02-22 22:27:04 +0900

    Update settings for deployment

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -1,14 +1,18 @@
 echo "build and pushing docker image"
+
+rm -fr ./rails/log/*
+rm -fr ./rails/tmp/*
+
 # build and push an image for rails
 docker build ./rails/ -t nuxt_on_rails_on_docker_rails
 cp ./rails/Dockerfile ./rails/Dockerfile.development
-cp -f production/rails/Dockerfile ./rails/Dockerfile
+cp -f kube/rails/Dockerfile ./rails/Dockerfile
 docker build ./rails/ -t gcr.io/gke-sample-201809/nuxt-rails-gke-sample
 mv -f ./rails/Dockerfile.development ./rails/Dockerfile
 docker push gcr.io/gke-sample-201809/nuxt-rails-gke-sample
 
 # build and push an image for nuxt
-cp -f production/nuxt/Dockerfile ./nuxt/Dockerfile
+cp -f kube/nuxt/Dockerfile ./nuxt/Dockerfile
 docker build ./nuxt/ -t gcr.io/gke-sample-201809/nuxt-rails-gke-sample-nuxt
 rm -f ./nuxt/Dockerfile
 docker push gcr.io/gke-sample-201809/nuxt-rails-gke-sample-nuxt
@@ -17,10 +21,10 @@ echo "beginnng deployment"
 kubectl delete deployments --all
 kubectl delete pods --all
 kubectl delete services --all
-kubectl create -f production/rails/deploy.yml
-kubectl create -f production/rails/service.yml
-kubectl create -f production/nuxt/deploy.yml
-kubectl create -f production/nuxt/service.yml
+kubectl create -f kube/rails/deploy.yml
+kubectl create -f kube/rails/service.yml
+kubectl create -f kube/nuxt/deploy.yml
+kubectl create -f kube/nuxt/service.yml
 
 echo ""
 echo "--- deployment complete! ---"
