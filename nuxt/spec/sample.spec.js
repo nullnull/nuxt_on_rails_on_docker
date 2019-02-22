747e7f7a88320759f097c109d42c7b77cb1545a5
Katsuma Narisawa <face.the.music721@gmail.com>
2019-02-22 18:14:23 +0900

    Improve DX (#8)
    * Install prettier
    
    * Install onchange
    
    * Install husky, lint-staged
    
    * Install jest related (by 'yarn add --dev jest vue-jest babel-jest @vue/test-utils babel-preset-vue-app @babel/core @babel/preset-env')
    
    * Configure jest
    
    * Install rubocop
    
    * Install guard-rubocop
    
    * Configure .rubocop.yml
    
    * rubocop -a
    
    * Run guard-rubocop as docker container

---
+++ nuxt/spec/sample.spec.js
@@ -0,0 +1,7 @@
+describe('sample.js', () => {
+  describe('hoge', () => {
+    test('fuga', () => {
+      expect(1).toBe(1)
+    })
+  })
+})
--- rails/db/migrate/20181006080935_create_users.rb
+++ rails/db/migrate/20181006080935_create_users.rb
@@ -1,8 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
-
-      t.timestamps
-    end
+    create_table :users, &:timestamps
   end
 end
